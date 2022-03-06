09f00b9ed98b91ae5d2c5c28c7e261a74212f55c
Intecom <Intecom@192.168.1.70>
2022-03-06 14:38:55 -0600

    actualizacion0.1

--- src/proyectoboutique/db/Conexion.java
+++ src/proyectoboutique/db/Conexion.java
@@ -12,9 +12,9 @@ import javax.swing.JOptionPane;
 public class Conexion {
     private final String DRIVER="com.mysql.cj.jdbc.Driver";
     private final String URL="jdbc:mysql://localhost:3306/";
-    private final String DB = "nombreparalabasededatos";
+    private final String DB = "basededatos";
     private final String USER = "root";
-    private final String PASSWORD = "";
+    private final String PASSWORD = "2002";
     
     public Connection cadena;
     public static Conexion instancia;
